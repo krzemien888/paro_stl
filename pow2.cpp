63e6a2fb39d805b2c287a1b21e60303a584d4579
paro <paro@localhost.localdomain>
2017-04-03 19:36:52 +0200

    End of class

---
+++ pow2.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+vector<T> pow2(vector<T> const& input)
+{
+    // TODO:      This function should return input elements raised to the power of two.
+    vector<T> o {};
+    
+    transform(input.begin(), input.end(), o.begin(), [](T a){return a*a;});
+    return vector<T>{};
+}
+
+TEST(Pow2Test, Test)
+{
+    vector<long> input =    {2, 5,  10,  15,  20,  25,  30,  45,   100};
+    vector<long> expected = {4, 25, 100, 225, 400, 625, 900, 2025, 10000};
+    auto actual = pow2(input);
+
+    ASSERT_EQ(expected, actual);
+}
+
+int main(int ac, char ** av)
+{
+    ::testing::InitGoogleTest(&ac, av);
+    return RUN_ALL_TESTS();
+}
+
